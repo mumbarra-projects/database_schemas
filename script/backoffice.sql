b270f0b999c481276f0b98c6bfd363c016b8c70e
Lucas Santos Mumbarra <lucassantos.contatoli@gmail.com>
2023-12-25 18:28:21 -0300

    Changed backoffice

--- script/backoffice.sql
+++ script/backoffice.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `company` (
-  `id` BIGINT NOT NULL,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `uuid` VARCHAR(45) NOT NULL,
   `name` VARCHAR(250) NOT NULL,
   `business_name` VARCHAR(250) NOT NULL,
@@ -11,13 +11,29 @@ CREATE TABLE IF NOT EXISTS `company` (
   `status` ENUM("ACTIVE", "INACTIVE") NOT NULL,
   `created_at` TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP NULL,
-  `deleted_at` TIMESTAMP NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
+  UNIQUE INDEX `document_UNIQUE` (`document` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(255) NULL,
+  `level` ENUM("FREE", "LIGHT", "PRO") NOT NULL,
+  `status` ENUM("ACTIVE", "INACTIVE") NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
+  UNIQUE INDEX `level_UNIQUE` (`level` ASC) VISIBLE,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` BIGINT NOT NULL,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `uuid` VARCHAR(45) NOT NULL,
   `name` VARCHAR(250) NOT NULL,
   `document` VARCHAR(14) NOT NULL,
@@ -29,9 +45,29 @@ CREATE TABLE IF NOT EXISTS `user` (
   `company_id` BIGINT NOT NULL,
   `created_at` TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP NULL,
-  `deleted_at` TIMESTAMP NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
   INDEX `fk_user_company_idx` (`company_id` ASC) VISIBLE,
+  UNIQUE INDEX `document_UNIQUE` (`document` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) INVISIBLE,
   CONSTRAINT `fk_company_user` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `company_service` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(45) NOT NULL,
+  `api_key` VARCHAR(45) NOT NULL,
+  `company_id` BIGINT NOT NULL,
+  `service_id` BIGINT NOT NULL,
+  `status` ENUM("PENDING", "ACTIVE", "BLOCKED", "CLOSED") NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
+  UNIQUE INDEX `api_key_UNIQUE` (`api_key` ASC) VISIBLE,
+  INDEX `fk_company_service_company_idx` (`company_id` ASC) VISIBLE,
+  INDEX `fk_company_service_service_idx` (`service_id` ASC) VISIBLE,
+  CONSTRAINT `fk_company_service_company` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_company_service_service` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
