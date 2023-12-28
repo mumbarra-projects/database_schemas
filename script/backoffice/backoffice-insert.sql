c044016999a029681817033e3ce1962621c27a9b
lucasmumbarra <lucassantosm.2014@gmail.com>
2023-12-28 17:59:44 -0300

    changed script db

---
+++ script/backoffice/backoffice-insert.sql
@@ -0,0 +1,131 @@
+INSERT INTO
+  `service`
+VALUES
+  (
+    1,
+    'SVC-c59e97d5-2ac8-40db-a27f-06503948195d',
+    'Grátis',
+    'Pacote grátis de serviços',
+    'FREE',
+    'YEARLY',
+    'ACTIVE',
+    '2023-12-28 20:43:01',
+    '2023-12-28 20:43:01'
+  ),
+  (
+    2,
+    'SVC-4357441e-0140-4bf1-9947-f6c029e3407a',
+    'Grátis',
+    'Pacote grátis de serviços',
+    'FREE',
+    'MONTHLY',
+    'ACTIVE',
+    '2023-12-28 20:47:17',
+    '2023-12-28 20:47:17'
+  ),
+  (
+    3,
+    'SVC-bee994d9-3c8d-4fb7-9a6d-b925378f3e78',
+    'Básico',
+    'Pacote básico de serviços',
+    'BASIC',
+    'YEARLY',
+    'ACTIVE',
+    '2023-12-28 20:47:52',
+    '2023-12-28 20:47:52'
+  ),
+  (
+    4,
+    'SVC-a627ced1-466f-40d3-a43d-92431ac25704',
+    'Básico',
+    'Pacote básico de serviços',
+    'BASIC',
+    'MONTHLY',
+    'ACTIVE',
+    '2023-12-28 20:48:05',
+    '2023-12-28 20:48:05'
+  ),
+  (
+    5,
+    'SVC-a6620ee0-1891-4d7b-b785-d4dae386399c',
+    'Essencial',
+    'Pacote essencial de serviços',
+    'ESSENTIAL',
+    'YEARLY',
+    'ACTIVE',
+    '2023-12-28 20:48:24',
+    '2023-12-28 20:48:24'
+  ),
+  (
+    6,
+    'SVC-a452ed55-b5c3-40aa-a917-3dc2ca509618',
+    'Essencial',
+    'Pacote essencial de serviços',
+    'ESSENTIAL',
+    'MONTHLY',
+    'ACTIVE',
+    '2023-12-28 20:48:31',
+    '2023-12-28 20:48:31'
+  ),
+  (
+    7,
+    'SVC-1225775a-633e-4a47-af2f-e8465c28094b',
+    'Completo',
+    'Pacote completo de serviços',
+    'COMPLETE',
+    'YEARLY',
+    'ACTIVE',
+    '2023-12-28 20:48:53',
+    '2023-12-28 20:48:53'
+  ),
+  (
+    8,
+    'SVC-89508f14-0394-4003-a7a1-3e21d3999eb6',
+    'Completo',
+    'Pacote completo de serviços',
+    'COMPLETE',
+    'MONTHLY',
+    'ACTIVE',
+    '2023-12-28 20:48:57',
+    '2023-12-28 20:48:57'
+  ),
+  (
+    9,
+    'SVC-1679984c-2c02-4d0a-820f-909f4075c17c',
+    'Premium',
+    'Pacote premium de serviços',
+    'PREMIUM',
+    'YEARLY',
+    'ACTIVE',
+    '2023-12-28 20:49:23',
+    '2023-12-28 20:49:23'
+  ),
+  (
+    10,
+    'SVC-eb58f8f4-00e8-4681-81c1-fe67ce714e4a',
+    'Premium',
+    'Pacote premium de serviços',
+    'PREMIUM',
+    'MONTHLY',
+    'ACTIVE',
+    '2023-12-28 20:49:27',
+    '2023-12-28 20:49:27'
+  );
+
+INSERT INTO
+  `company`
+VALUES
+  (
+    1,
+    'COP-e308d879-5145-4402-9073-16d8ff21187c',
+    'Admin',
+    'Admin',
+    '11222333000144',
+    NULL,
+    NULL,
+    NULL,
+    'lucassantosm.2014@gmail.com',
+    'ACTIVE',
+    '2023-12-27 16:50:51',
+    '2023-12-28 16:04:52'
+  );
